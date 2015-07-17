1dffddfef19d8a8036324998e55bbb140eb9c13d
noprom <tyee.noprom@qq.com>
2015-07-17 19:44:53 +0800

    init conversation list controller

--- CloudTMTest/ConversationListViewController.swift
+++ CloudTMTest/ConversationListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ConversitionViewController.swift
+//  ConversationListViewController.swift
 //  CloudTMTest
 //
 //  Created by noprom on 15/7/17.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ConversationViewController: RCConversationViewController {
+class ConversationListViewController: RCConversationListViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 初始化聊天的对象
-        self.targetId = RCIMClient.sharedRCIMClient().currentUserInfo.userId
-        self.userName = RCIMClient.sharedRCIMClient().currentUserInfo.name
-        self.conversationType = RCConversationType.ConversationType_PRIVATE // 私聊
-        self.title = "与" + self.userName + "对话中"
-        
-        self.setMessageAvatarStyle(RCUserAvatarStyle._USER_AVATAR_CYCLE) // 设置圆形边角
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
